2e9b8655a7a4126b8b6bcddf1a698c4f322a7edb
PragathiRao26 <83211453+PragathiRao26@users.noreply.github.com>
2024-03-21 12:00:47 +0530

    new

--- academic_records.sql
+++ academic_records.sql
@@ -5,7 +5,6 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-
 /*!40101 SET NAMES utf8mb4 */;
 
 --
@@ -15,11 +14,12 @@ CREATE DATABASE IF NOT EXISTS `academic_records` DEFAULT CHARACTER SET utf8mb4 C
 USE `academic_records`;
 
 -- --------------------------------------------------------
+
+--
 -- Table structure for table `userdata`
 --
 
 DROP TABLE IF EXISTS `userdata`;
-
 CREATE TABLE `userdata` (
   `sno` int(11) NOT NULL AUTO_INCREMENT,
   `Emailid` varchar(50) NOT NULL,
@@ -38,35 +38,22 @@ INSERT INTO `userdata` (`sno`, `Emailid`, `Password`, `Type`, `name`) VALUES
 (2, 'teacher@somaiya.edu', 'teacher123', 'teacher', 'Latha'),
 (3, 'student@somaiya.edu', 'student123', 'student', 'Bhavya');
 
---
--- Indexes for table `userdata`
---
-ALTER TABLE `userdata`
-  MODIFY `sno` int(11) NOT NULL AUTO_INCREMENT;
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `todolist`
 --
 
 DROP TABLE IF EXISTS `todolist`;
-
-CREATE TABLE `todolist` (
-    `id` INT AUTO_INCREMENT PRIMARY KEY,
-    `info` VARCHAR(255),
-    `date` DATE,
-    `title` VARCHAR(100),
-    `username` VARCHAR(50)
-);
-DROP TABLE IF EXISTS `todolist`;
-
 CREATE TABLE `todolist` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `info` text NOT NULL,
-  `date` date NOT NULL,
   `title` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `priority` varchar(50) NOT NULL,
+  `due_date` date NOT NULL,
   `username` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 COMMIT;
 
