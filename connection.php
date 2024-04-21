1d220859aefa27a65279e12fb0c644f5ddf07ba8
Minank Mehta <112801417+MinankMehta@users.noreply.github.com>
2024-04-21 22:50:54 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -2,7 +2,7 @@
 $servername="localhost";
 $username="root";
 $password="";
-$dbname="wp_mini";
+$dbname="academic_records";
 $conn=mysqli_connect($servername,$username,$password,$dbname);
 if(!$conn)
 {
